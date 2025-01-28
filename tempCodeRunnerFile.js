680124c0d3e7388bb03d7908d2c083c2bb439250
Tuhin Al Rakib <129104225+tuhinalrakib@users.noreply.github.com>
2025-01-29 02:30:06 +0600

    Update tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,12 +1,1 @@
-uniqueBtn.addEventListener("click",(e)=>{
-//     output.innerHTML = ""
-//     for(let i=0;i<friends.length;i++){
-//         for(let j=1;j<=friends[i].length;j++){
-//             if(friends[i] === friends[i]){
-//                 // friends[i] = friends[i].slice(0, j-1) + friends[i].slice(j)
-//                 console.log(friends[i])
-//                 console.log(friends[j])
-//             }
-//         }
-//     }
-// })
+
